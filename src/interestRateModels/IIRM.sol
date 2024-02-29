b64b3f730a9ba909b33bccaf7266731cecbb6b96
dglowinski <dariushg32111@gmail.com>
2024-02-29 10:49:26 +0100

    nuke

--- src/interestRateModels/IIRM.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-pragma solidity ^0.8.0;
-
-interface IIRM {
-    function computeInterestRate(address market, address asset, uint32 utilisation) external returns (uint256);
-    function reset(address market, bytes calldata resetParams) external;
-}
